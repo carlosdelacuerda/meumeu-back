968dc3601e3042e6ae63b448a56eba8ebb3b431f
Carlos de la Cuerda <carlosdelacuerda@gmail.com>
2021-03-30 09:22:44 +0200

    envío

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+console.log('market-1')
 const createError = require('http-errors');
+console.log('market0')
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -8,22 +10,27 @@ const cors = require('cors');
 // Conexión Base de datos
 require('./dbConfig')();
 
-
+console.log('market1')
 
 
 
 const indexRouter = require('./routes/index');
+console.log('market2')
 const usersRouter = require('./routes/users');
+console.log('market3')
 const tripsRouter = require('./routes/trips');
+console.log('market4')
 const apiRouter = require('./routes/api');
-
+const { deleteById } = require('./models/users');
+console.log('market5')
 
 var app = express();
-
+console.log('market6')
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+console.log('market7')
 app.set('view engine', 'pug');
-
+console.log('market8')
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
@@ -36,8 +43,12 @@ app.use('/users', usersRouter);
 app.use('/trips', tripsRouter);
 app.use('/api', apiRouter);
 
+
+console.log(app.stack);
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
+  console.log(req.hostname)
   next(createError(404));
 });
 
--- dbConfig.js
+++ dbConfig.js
@@ -1,15 +1,38 @@
-const mysql = require('mysql');
+// const mysql = require('mysql');
 
-// Conectar con la BD cuando arranque la APP
+// // Conectar con la BD cuando arranque la APP
+// const connect = () => {
+//     const pool = mysql.createPool({
+//         host: '127.0.0.1',
+//         user: 'root',
+//         password: 'root',
+//         port: 8889,
+//         database: 'meumeu'
+//     });
+//     global.db = pool;
+// }
+
+// module.exports = connect;
+
+const mysql = require('mysql');
 const connect = () => {
+    console.log('conexión base de datos');
     const pool = mysql.createPool({
-        host: '127.0.0.1',
-        user: 'root',
-        password: 'root',
-        port: 8889,
-        database: 'meumeu'
+        host: 'localhost',
+        user: 'meumeues_root',
+        password: 'amor6969',
+        port: 3306,
+        database: 'meumeues_db'
     });
     global.db = pool;
 }
+
+// con.connect(function(err) {
+
+//     if (err) throw err;
+  
+//     console.log("Connected!");
+  
+//   });
 
 module.exports = connect;
